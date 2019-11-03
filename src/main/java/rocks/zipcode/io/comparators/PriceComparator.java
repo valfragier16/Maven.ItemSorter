c41dcf13d36ea5d1df988e0545d6d7e08fea8945
val <valfragier@gmail.com>
2019-11-03 15:06:43 -0500

    Complete Lab

--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,14 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item> {
+    public int compare(Item item1, Item item2){
+        return item1.getPrice().compareTo(item2.getPrice());
+    }
 }
